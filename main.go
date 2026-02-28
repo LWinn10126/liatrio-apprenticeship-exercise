da495c5688636ab9e1b022cb02e2b1035a5d168f
LWinn10126 <lwinn@csuchico.edu>
2026-02-28 12:26:06 -0800

    Fixed JSON spelling && time stamp format

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	//data := { "message": "Logan Winn","timestamp": ""}
 
 	app.Get("/", func(c fiber.Ctx) error { // Define a route for the root URL  c fiber.Ctx is the request context for headers, parameters, etc.
-		err := c.JSON(fiber.Map{"message": "My name is Logan Winn", "timestamp": time.Now().Format("2006-01-02 15:04:05")}) // Send a simple string response
+		err := c.JSON(fiber.Map{"Message": "My name is Logan Winn", "Timestamp": time.Now().Unix()}) // Send a simple string response
 		return err
 	})
 
