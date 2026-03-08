2731c1f0701082f27507b3b9e4ee9e0e49f70c1b
LWinn10126 <lwinn@csuchico.edu>
2026-03-08 14:21:51 -0700

    Adding deploy.yml to push to cloud run

--- main.go
+++ main.go
@@ -10,11 +10,11 @@ import (
 func main() {
 	app := fiber.New() // Create a new Fiber application
 
-	//data := { "message": "Logan Winn","timestamp": ""}
-
 	app.Get("/", func(c fiber.Ctx) error { // Define a route for the root URL  c fiber.Ctx is the request context for headers, parameters, etc.
-		err := c.JSON(fiber.Map{"message": "My name is Logan Winn", "timestamp": time.Now().UnixMilli()}) // Send a simple string response with time allocation
-		return err
+		version := os.Getenv("APP_VERSION") // Get the APP_VERSION environment variable
+
+		// Send a simple string response with time allocation and version
+		return c.JSON(fiber.Map{"message": "My name is Logan Winn", "timestamp": time.Now().UnixMilli(), "version": version})
 	})
 
 	// Get port from environment (Cloud Run sets this automatically)
